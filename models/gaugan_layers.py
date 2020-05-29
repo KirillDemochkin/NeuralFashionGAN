94c84cffebed88fc668d6f2344449ab1af0d2817
Kirill Demochkin <dravkir@mail.ru>
2020-05-30 02:38:34 +0300

    spectral norm for D and G

--- models/gaugan_layers.py
+++ models/gaugan_layers.py
@@ -7,10 +7,10 @@ class SPADE(nn.Module):
         super(SPADE, self).__init__()
         self.scale_factor = scale_factor
         self.bn = nn.BatchNorm2d(n_filters)
-        self.shared_conv = nn.Sequential(nn.Conv2d(mask_channels, 128, kernel_size=3, padding=1),
+        self.shared_conv = nn.Sequential(nn.utils.spectral_norm(nn.Conv2d(mask_channels, 128, kernel_size=3, padding=1)),
                                          nn.ReLU(inplace=True))
-        self.mu_conv = nn.Conv2d(128, n_filters, kernel_size=3, padding=1)
-        self.sigma_conv = nn.Conv2d(128, n_filters, kernel_size=3, padding=1)
+        self.mu_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1))
+        self.sigma_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1))
 
     def forward(self, x, mask):
         mask = nn.functional.interpolate(mask, scale_factor=self.scale_factor)
@@ -25,14 +25,13 @@ class SPADE_ResBlock(nn.Module):
         super(SPADE_ResBlock, self).__init__()
         self.spade_1 = SPADE(scale_factor, in_filters, mask_channels)
         self.relu_1 = nn.ReLU(inplace=True)
-        self.conv_1 = nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1)
+        self.conv_1 = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
         self.spade_2 = SPADE(scale_factor, n_filters, mask_channels)
         self.relu_2 = nn.ReLU(inplace=True)
         self.conv_2 = nn.Conv2d(n_filters, n_filters, kernel_size=3, padding=1)
         self.spade_skip = SPADE(scale_factor, in_filters, mask_channels)
         self.relu_skip = nn.ReLU(inplace=True)
-        self.conv_skip = nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1)
-
+        self.conv_skip = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
 
     def forward(self, x, mask):
         out = self.conv_1(self.relu_1(self.spade_1(x, mask)))
@@ -44,7 +43,7 @@ class SPADE_ResBlock(nn.Module):
 class Discriminator_block(nn.Module):
     def __init__(self, in_filters, out_filters, normalization=True):
         super(Discriminator_block, self).__init__()
-        self.conv = nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)
+        self.conv = nn.utils.spectral_norm(nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1))
         self.norm = nn.InstanceNorm2d(out_filters)
         self.leaky = nn.LeakyReLU(0.2, inplace=True)
         self.normalization = normalization
