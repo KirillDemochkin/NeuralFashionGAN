0b9286e8564a2aeeff6bff843cdd02141a3d6531
Kirill Demochkin <dravkir@mail.ru>
2020-05-30 15:35:22 +0300

    changed weights init to glorot uniform

--- utils/weights_init.py
+++ utils/weights_init.py
@@ -6,9 +6,9 @@ def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         #nn.init.normal_(m.weight.data, 0.0, 0.02)
-        nn.init.xavier_normal_(m.weight.data, gain=0.02)
+        nn.init.xavier_uniform_(m.weight.data, gain=0.02)
     elif classname.find('Linear') != -1:
-        nn.init.xavier_normal_(m.weight.data, gain=0.02)
+        nn.init.xavier_uniform_(m.weight.data, gain=0.02)
         #nn.init.normal_(m.weight.data, 0.0, 0.02)
         nn.init.constant_(m.bias.data, 0.0)
     elif classname.find('BatchNorm') != -1:
