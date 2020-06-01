cbbd1185985970d3f376539a2ba62890ecac65b5
Kirill Demochkin <dravkir@mail.ru>
2020-06-01 22:49:28 +0300

    train_loop full coco

--- run_train_test.py
+++ run_train_test.py
@@ -83,25 +83,26 @@ coco_images_dir = str(args.data_root) + '/coco-stuff/val_images2017/'
 coco_masks_dir = str(args.data_root) + '/coco-stuff/val_labels2017/'
 coco_images_files = os.listdir(coco_images_dir)
 coco_images_files.sort()
-coco_train_images_files = coco_images_files[:4000]
-coco_val_images_files = coco_images_files[4000:4500]
-coco_test_images_files = coco_images_files[4500:5000]
+coco_train_images_files = coco_images_files
+#coco_val_images_files = coco_images_files[4000:4500]
+#coco_test_images_files = coco_images_files[4500:5000]
 coco_labels_num = 182
 # train_dataset = DeepFashion2Dataset(train_images_dir,  train_coco_annos)
 # val_dataset = DeepFashion2Dataset(validation_images_dir, validation_coco_annos)
 coco_train_dataset = CocoDataset(coco_images_dir, coco_masks_dir, coco_train_images_files, coco_labels_num)
-coco_val_dataset = CocoDataset(coco_images_dir, coco_masks_dir, coco_val_images_files, coco_labels_num)
-coco_test_dataset = CocoDataset(coco_images_dir, coco_masks_dir, coco_test_images_files, coco_labels_num)
+#coco_val_dataset = CocoDataset(coco_images_dir, coco_masks_dir, coco_val_images_files, coco_labels_num)
+#coco_test_dataset = CocoDataset(coco_images_dir, coco_masks_dir, coco_test_images_files, coco_labels_num)
 
 print('Loading Dataset...')
 sys.stdout.flush()
 train_loader = data_utils.DataLoader(coco_train_dataset, batch_size=args.batch_size, shuffle=True)
-val_loader = data_utils.DataLoader(coco_val_dataset, batch_size=args.batch_size, shuffle=True)
-test_loader = data_utils.DataLoader(coco_test_dataset, batch_size=args.batch_size, shuffle=True)
+#val_loader = data_utils.DataLoader(coco_val_dataset, batch_size=args.batch_size, shuffle=True)
+#est_loader = data_utils.DataLoader(coco_test_dataset, batch_size=args.batch_size, shuffle=True)
 
-test_batch = next(iter(val_loader))
+test_batch = next(iter(train_loader))
 fixed_test_images = test_batch[0].to(device)
 fixed_test_masks= test_batch[1].to(device)
+_ = vutils.save_image(fixed_test_images.cpu().data[:16], '!test.png', normalize=True)
 
 ##MODE
 netD = MultiscaleDiscriminator(args.mask_channels + 3).to(device)
