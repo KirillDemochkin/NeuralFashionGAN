7800af732b2edcc27e83d63a2cc7d2cff105cfd8
Kirill Demochkin <dravkir@mail.ru>
2020-06-01 03:38:13 +0300

    StyleGAN + SPADE

--- models/gaugan_layers.py
+++ models/gaugan_layers.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from .stylegan_layers import AdaIN
 
 
 class SPADE(nn.Module):
@@ -37,6 +38,44 @@ class SPADE_ResBlock(nn.Module):
         out = self.conv_1(self.relu_1(self.spade_1(x, mask)))
         out = self.conv_2(self.relu_2(self.spade_2(out, mask)))
         x = self.conv_skip(self.relu_skip(self.spade_skip(x, mask)))
+        return out + x
+
+
+class Style_SPADE(nn.Module):
+    def __init__(self, scale_factor, n_filters, mask_channels, latent_dim):
+        super(Style_SPADE, self).__init__()
+        self.scale_factor = scale_factor
+        self.bn = AdaIN(latent_dim, n_filters)
+        self.shared_conv = nn.Sequential(nn.utils.spectral_norm(nn.Conv2d(mask_channels, 128, kernel_size=3, padding=1)),
+                                         nn.ReLU(inplace=True))
+        self.mu_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1))
+        self.sigma_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1))
+
+    def forward(self, x, mask, style_code):
+        mask = nn.functional.interpolate(mask, scale_factor=self.scale_factor)
+        conditional_features = self.shared_conv(mask)
+        mu = self.mu_conv(conditional_features)
+        sigma = self.sigma_conv(conditional_features)
+        return (self.bn(x, style_code) * sigma) + mu
+
+
+class Style_SPADE_ResBlock(nn.Module):
+    def __init__(self, scale_factor, in_filters, n_filters , mask_channels, latent_dim):
+        super(Style_SPADE_ResBlock, self).__init__()
+        self.spade_1 = Style_SPADE(scale_factor, in_filters, mask_channels, latent_dim)
+        self.relu_1 = nn.ReLU(inplace=True)
+        self.conv_1 = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
+        self.spade_2 = Style_SPADE(scale_factor, n_filters, mask_channels, latent_dim)
+        self.relu_2 = nn.ReLU(inplace=True)
+        self.conv_2 = nn.Conv2d(n_filters, n_filters, kernel_size=3, padding=1)
+        self.spade_skip = Style_SPADE(scale_factor, in_filters, mask_channels, latent_dim)
+        self.relu_skip = nn.ReLU(inplace=True)
+        self.conv_skip = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
+
+    def forward(self, x, mask, style_code):
+        out = self.conv_1(self.relu_1(self.spade_1(x, mask, style_code)))
+        out = self.conv_2(self.relu_2(self.spade_2(out, mask, style_code)))
+        x = self.conv_skip(self.relu_skip(self.spade_skip(x, mask, style_code)))
         return out + x
 
 
--- models/stylegan_layers.py
+++ models/stylegan_layers.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-
 class AdaIN(nn.Module):
     def __init__(self, latent_size, num_ch):
         super(AdaIN, self).__init__()
@@ -13,3 +12,28 @@ class AdaIN(nn.Module):
         mu = self.mu_fc(a)
         sigma = self.sigma_fc(a).exp()
         return (x * sigma) + mu
+
+
+class StylizationNoiseNetwork(nn.Module):
+    def __init__(self, num_channels):
+        super(StylizationNoiseNetwork, self).__init__()
+
+        self.B1 = torch.empty((1, num_channels[0], 1, 1)).normal_(0, 0.02)
+        self.B1.requires_grad_(True)
+
+        self.B2 = torch.empty((1, num_channels[1], 1, 1)).normal_(0, 0.02)
+        self.B2.requires_grad_(True)
+
+        self.B3 = torch.empty((1, num_channels[2], 1, 1)).normal_(0, 0.02)
+        self.B3.requires_grad_(True)
+
+        self.B4 = torch.empty((1, num_channels[3], 1, 1)).normal_(0, 0.02)
+        self.B4.requires_grad_(True)
+
+
+    def forward(self, x):
+        scaled_noise_1 = torch.mul(x[0], self.B1)
+        scaled_noise_2 = torch.mul(x[1], self.B2)
+        scaled_noise_3 = torch.mul(x[2], self.B3)
+        scaled_noise_4 = torch.mul(x[3], self.B4)
+        return scaled_noise_1, scaled_noise_2, scaled_noise_3, scaled_noise_4
