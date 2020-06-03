a2db6e3e7ac0964c109dbb7e012d1f6338995040
polina <buzoilic@gmail.com>
2020-06-03 23:22:21 +0300

    added orthogonal  weight initialization

--- utils/weights_init.py
+++ utils/weights_init.py
@@ -6,11 +6,11 @@ def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         #nn.init.normal_(m.weight.data, 0.0, 0.02)
-        nn.init.xavier_uniform_(m.weight.data, gain=0.02)
+        nn.init.orthogonal_(m.weight.data, gain=0.0001)
     elif classname.find('Linear') != -1:
-        nn.init.xavier_uniform_(m.weight.data, gain=0.02)
+        nn.init.orthogonal_(m.weight.data, gain=0.0001)
         #nn.init.normal_(m.weight.data, 0.0, 0.02)
         nn.init.constant_(m.bias.data, 0.0)
     elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.orthogonal_(m.weight.data, gain = 0.0001)
         nn.init.constant_(m.bias.data, 0.0)
