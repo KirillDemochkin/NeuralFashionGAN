8fc183e7628c2c482b56412a232475fbaa8b857b
Kirill Demochkin <dravkir@mail.ru>
2020-06-03 04:17:30 +0300

    spectral norm stability

--- models/gaugan_layers.py
+++ models/gaugan_layers.py
@@ -26,13 +26,13 @@ class SPADE_ResBlock(nn.Module):
         super(SPADE_ResBlock, self).__init__()
         self.spade_1 = SPADE(scale_factor, in_filters, mask_channels)
         self.relu_1 = nn.ReLU(inplace=True)
-        self.conv_1 = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
+        self.conv_1 = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1), eps=1e-6)
         self.spade_2 = SPADE(scale_factor, n_filters, mask_channels)
         self.relu_2 = nn.ReLU(inplace=True)
         self.conv_2 = nn.Conv2d(n_filters, n_filters, kernel_size=3, padding=1)
         self.spade_skip = SPADE(scale_factor, in_filters, mask_channels)
         self.relu_skip = nn.ReLU(inplace=True)
-        self.conv_skip = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
+        self.conv_skip = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1), eps=1e-6)
 
     def forward(self, x, mask):
         out = self.conv_1(self.relu_1(self.spade_1(x, mask)))
@@ -46,10 +46,10 @@ class Style_SPADE(nn.Module):
         super(Style_SPADE, self).__init__()
         self.scale_factor = scale_factor
         self.bn = AdaIN(latent_dim, n_filters)
-        self.shared_conv = nn.Sequential(nn.utils.spectral_norm(nn.Conv2d(mask_channels, 128, kernel_size=3, padding=1)),
+        self.shared_conv = nn.Sequential(nn.utils.spectral_norm(nn.Conv2d(mask_channels, 128, kernel_size=3, padding=1), eps=1e-6),
                                          nn.ReLU(inplace=True))
-        self.mu_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1))
-        self.sigma_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1))
+        self.mu_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1), eps=1e-6)
+        self.sigma_conv = nn.utils.spectral_norm(nn.Conv2d(128, n_filters, kernel_size=3, padding=1), eps=1e-6)
 
     def forward(self, x, mask, style_code):
         mask = nn.functional.interpolate(mask, scale_factor=self.scale_factor)
@@ -65,13 +65,13 @@ class Style_SPADE_ResBlock(nn.Module):
         super(Style_SPADE_ResBlock, self).__init__()
         self.spade_1 = Style_SPADE(scale_factor, in_filters, mask_channels, latent_dim)
         self.relu_1 = nn.ReLU(inplace=True)
-        self.conv_1 = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
+        self.conv_1 = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1), eps=1e-6)
         self.spade_2 = Style_SPADE(scale_factor, n_filters, mask_channels, latent_dim)
         self.relu_2 = nn.ReLU(inplace=True)
-        self.conv_2 = nn.utils.spectral_norm(nn.Conv2d(n_filters, n_filters, kernel_size=3, padding=1))
+        self.conv_2 = nn.utils.spectral_norm(nn.Conv2d(n_filters, n_filters, kernel_size=3, padding=1), eps=1e-6)
         self.spade_skip = Style_SPADE(scale_factor, in_filters, mask_channels, latent_dim)
         self.relu_skip = nn.ReLU(inplace=True)
-        self.conv_skip = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1))
+        self.conv_skip = nn.utils.spectral_norm(nn.Conv2d(in_filters, n_filters, kernel_size=3, padding=1), eps=1e-6)
 
     def forward(self, x, mask, style_code):
         out = self.conv_1(self.relu_1(self.spade_1(x, mask, style_code)))
@@ -83,7 +83,7 @@ class Style_SPADE_ResBlock(nn.Module):
 class Discriminator_block(nn.Module):
     def __init__(self, in_filters, out_filters, normalization=True):
         super(Discriminator_block, self).__init__()
-        self.conv = nn.utils.spectral_norm(nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1))
+        self.conv = nn.utils.spectral_norm(nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1), eps=1e-6)
         self.norm = nn.InstanceNorm2d(out_filters)
         self.leaky = nn.LeakyReLU(0.2, inplace=True)
         self.normalization = normalization
