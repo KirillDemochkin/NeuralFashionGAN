923f2c2fc69bfd224075d37268eef5e8fdb2c981
polina <buzoilic@gmail.com>
2020-06-05 17:22:41 +0300

    added orthogonal regularization

--- utils/losses.py
+++ utils/losses.py
@@ -22,3 +22,14 @@ def perceptual_loss(fake_feats, real_feats, lmbda):
 
 def masked_l1(predict, target, mask):
     return torch.sum(torch.abs((predict-target)*mask)) / torch.sum(mask)
+
+def ortho(model, strength=1e-4, blacklist=[]):
+        with torch.no_grad():
+            for param in model.parameters():
+                # Only apply this to parameters with at least 2 axes, and not in the blacklist
+                if len(param.shape) < 2 or any([param is item for item in blacklist]):
+                    continue
+                w = param.view(param.shape[0], -1)
+                grad = (2 * torch.mm(torch.mm(w, w.t())
+                                     * (1. - torch.eye(w.shape[0], device=w.device)), w))
+                param.grad.data += strength * grad.view(param.shape)
