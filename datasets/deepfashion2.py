0ba6d8d5722160340ee813f703d13a44832a8292
Kirill Demochkin <dravkir@mail.ru>
2020-06-06 02:58:25 +0300

    unfrms

--- datasets/deepfashion2.py
+++ datasets/deepfashion2.py
@@ -65,7 +65,7 @@ class DeepFashion2Dataset(Dataset):
             masked_image[1, m] = average_color[1]
             masked_image[2, m] = average_color[2]
             if self.noise:
-                noise = torch.rand_like(masked_image)
+                noise = torch.zeros_like(masked_image).uniform_(-0.1, 0.1)
                 noise[:, full_mask.sum(dim=0) <= 0] = 0.
                 masked_image += noise
                 #masked_image = torch.clamp(masked_image, -1, 1)
