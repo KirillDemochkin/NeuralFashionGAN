3e0c0bb0ae61fa754a600828bcf3e5502216f5ff
Kirill Demochkin <dravkir@mail.ru>
2020-06-05 13:58:57 +0300

    changed noise to single map fro broadcasting

--- models/gaugan_generators.py
+++ models/gaugan_generators.py
@@ -142,18 +142,18 @@ class GauGANUnetStylizationGenerator(nn.Module):
 
     def forward(self, style_vec, mask, skips):
         style_code = self.mapping_net(style_vec)
-        style_noise = self.noise_net([torch.randn(style_code.shape[0], self.latent_dim * 4 + self.skip_dim, self.initial_image_size, self.initial_image_size, device=self.device),
-                                      torch.randn(style_code.shape[0], self.latent_dim * 4 + self.skip_dim, self.initial_image_size*2, self.initial_image_size*2, device=self.device),
-                                      torch.randn(style_code.shape[0], self.latent_dim * 4 + self.skip_dim, self.initial_image_size*4, self.initial_image_size*4, device=self.device),
-                                      torch.randn(style_code.shape[0], self.latent_dim * 4 + self.skip_dim, self.initial_image_size*8, self.initial_image_size*8, device=self.device),
-                                      torch.randn(style_code.shape[0], self.latent_dim * 2 + self.skip_dim, self.initial_image_size*16, self.initial_image_size*16, device=self.device),
-                                      torch.randn(style_code.shape[0], self.latent_dim + self.skip_dim, self.initial_image_size*32, self.initial_image_size*32, device=self.device)])
+        style_noise = self.noise_net([torch.randn(style_code.shape[0], 1, self.initial_image_size, self.initial_image_size, device=self.device),
+                                      torch.randn(style_code.shape[0], 1, self.initial_image_size*2, self.initial_image_size*2, device=self.device),
+                                      torch.randn(style_code.shape[0], 1, self.initial_image_size*4, self.initial_image_size*4, device=self.device),
+                                      torch.randn(style_code.shape[0], 1, self.initial_image_size*8, self.initial_image_size*8, device=self.device),
+                                      torch.randn(style_code.shape[0], 1, self.initial_image_size*16, self.initial_image_size*16, device=self.device),
+                                      torch.randn(style_code.shape[0], 1, self.initial_image_size*32, self.initial_image_size*32, device=self.device)])
 
         x = self.starting_noise.repeat(style_vec.shape[0], 1)
         x = self.linear(x+torch.zeros_like(x).uniform_(-0.05, 0.05))
         #x = self.linear(x)
         x = torch.reshape(x, (-1, self.latent_dim*4, self.initial_image_size, self.initial_image_size))
-        x = self.upsample_1(self.spd_blck_1(torch.cat((x, skips[5]), dim=1) + style_noise[0], mask, style_code))
+        x = self.upsample_1(self.spd_blck_1(torch.cat((x, skips[5]), dim=1), mask, style_code))
         x = self.upsample_2(self.spd_blck_2(torch.cat((x, skips[4]), dim=1) + style_noise[1], mask, style_code))
         x = self.upsample_3(self.spd_blck_3(torch.cat((x, skips[3]), dim=1) + style_noise[2], mask, style_code))
         x = self.upsample_4(self.spd_blck_4(torch.cat((x, skips[2]), dim=1) + style_noise[3], mask, style_code))
