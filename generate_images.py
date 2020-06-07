7acc50aa63c22f5848c64dcc00351d52a3e84472
Maria <36924974+MariBax@users.noreply.github.com>
2020-06-07 21:23:18 +0300

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -1,12 +1,14 @@
 import argparse
 import torch
-from datasets.deepfashion2 import DeepFashion2Dataset
+import cv2
+import numpy as np
+from PIL import Image
+# from datasets.deepfashion2 import DeepFashion2Dataset
 from datasets.style_dataset import StyleDataset
 from datasets.custom_dataset import CustomDataset
-from utils import visualization as vutils
 
 from models.gaugan_generators import GauGANUnetStylizationGenerator
-from models.encoders import StyleEncoder
+from models.encoders import StyleEncoder, MappingNetwork
 
 from albumentations import (
     Compose,
@@ -27,10 +29,92 @@ parser.add_argument('--encoder_latent_dim', default=256, type=float)
 parser.add_argument('--unet_ch', default=4, type=float)
 parser.add_argument('--max_num', help='maximum number of examples to process', default=10, type=int)
 parser.add_argument('--test_image_dir', help='path to where to save generated images', type=str, default='results')
-parser.add_argument('--weights_generator', help='path to generator model', type=str, default='trained_models/NetGDeepFashionStyleGAN.best.pth')
-parser.add_argument('--weights_style_encoder', help='path to style encoder', type=str, default='trained_models/NetSDeepFashionStyleGAN.best.pth')
+parser.add_argument('--weights_generator', help='path to generator model', type=str, default='trained_models/NetGDeepFashionStyleGANImproved.best.pth')
+parser.add_argument('--weights_style_encoder', help='path to style encoder', type=str, default='trained_models/NetSDeepFashionStyleGANImproved.best.pth')
+parser.add_argument('--weights_style_mapping', help='path to mapping network', type=str, default='trained_models/NetMDeepFashionStyleGANImproved.best.pth')
 
 args = parser.parse_args()
+
+import json
+from pycocotools import mask as maskUtils
+
+def ann_to_mask(segm, h, w):
+    rles = maskUtils.frPyObjects(segm, h, w)
+    rle = maskUtils.merge(rles)
+    m = maskUtils.decode(rle)
+    return m
+
+# DeepFashion2Dataset modified for convinient generation of images
+class DeepFashion2Dataset(Dataset):
+    def __init__(self, root, num_classes=13, transform=None, return_masked_image=False, noise = False):
+        super().__init__()
+
+        self.image_folder = join(root, 'image')
+        self.annos_folder = join(root, 'annos')
+        self.length = len(os.listdir(self.image_folder))
+        self.num_classes = num_classes
+        self.transform = transform
+        self.return_masked_image = return_masked_image
+        self.noise = noise
+
+    def get_item(self, idx, average_color=None):
+        name = f'{idx + 1:06d}'
+
+        image_path = join(self.image_folder, name + '.jpg')
+        image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+        image = image.astype(np.float32) / 255.
+
+        width, height = image.shape[1], image.shape[0]
+
+        ann_path = join(self.annos_folder, name + '.json')
+        with open(ann_path) as f:
+            ann = json.load(f)
+
+        full_mask = np.zeros((height, width, self.num_classes),
+                             dtype=np.float32)
+        for key, item in ann.items():
+            if key in ['source', 'pair_id']:
+                continue
+            clid = item['category_id']
+            mask = ann_to_mask(item['segmentation'], height, width)
+            full_mask[mask > 0, clid - 1] = 1
+
+        if self.transform is not None:
+            augmented = self.transform(image=image, mask=full_mask)
+            image = augmented['image']
+            full_mask = augmented['mask'].permute(2, 0, 1)
+
+        image *= 2
+        image -= 1
+
+        if self.return_masked_image:
+            masked_image = image.clone()
+
+            if average_color is None:
+                average_color = torch.mean(masked_image.view(3, -1), dim=-1)
+            m = full_mask.sum(dim=0) > 0
+            masked_image[0, m] = average_color[0]
+            masked_image[1, m] = average_color[1]
+            masked_image[2, m] = average_color[2]
+            # masked_image[:, m] = 1.0
+            if self.noise:
+                noise = torch.zeros_like(masked_image).uniform_(-0.1, 0.1)
+                noise[:, full_mask.sum(dim=0) <= 0] = 0.
+                masked_image += noise
+                masked_image = torch.clamp(masked_image, -1, 1)
+            loss_mask = torch.ones_like(masked_image)
+            loss_mask[:, full_mask.sum(dim=0) > 0] = 0.
+            return image, full_mask, masked_image, loss_mask
+        else:
+            return image, full_mask
+
+    def __len__(self):
+        return self.length
+
+
+def image_to_plot(image):
+    image_p = (np.transpose(image, [1, 2, 0]) + 1 / 2)
+    return image_p
 
 def generate_images():
 
@@ -46,10 +130,10 @@ def generate_images():
     # create dataset
     if args.test_dataset_name == 'deepfashion2':
         dataset = DeepFashion2Dataset(
-            args.dataset_dir, transform=transform, return_masked_image=True)
+            args.dataset_dir, transform=transform, return_masked_image=True, noise=True)
     else:
         dataset = CustomDataset(
-            args.dataset_dir, transform=transform, return_masked_image=True)
+            args.dataset_dir, transform=transform, return_masked_image=True, noise=True)
 
     print('...Length of dataset:', len(dataset))
 
@@ -70,30 +154,47 @@ def generate_images():
     netS.load_state_dict(state_dict_style_encoder)
     netS.to(device)
 
+    # load mapping network
+    netM = MappingNetwork(args.encoder_latent_dim)
+    state_dict_mapping= torch.load(args.weights_mapping, map_location=device)
+    netM.load_state_dict(state_dict_mapping)
+    netM.to(device)
+
     print('...Started generating images')
 
     # start generating clothes
     for i in range(1, min(len(dataset), args.max_num)):
-        # take one dataset example
-        image, mask, masked_image, _ = dataset[i]
+
+        # take style image
+        style_image = styles_dataset.get_image()
+        style_image_net = style_image.unsqueeze(0).to(device)
+
+        average_color = torch.mean(style_image.view(3, -1), dim=-1)
+        # print(average_color)
+
+        # take dataset image
+        image, mask, masked_image, _ = dataset.get_item(1, average_color)
 
         # make it like-batch
-        image_ = image.unsqueeze(0).to(device)
-        mask_ = mask.unsqueeze(0).to(device)
-        masked_image_ = masked_image.unsqueeze(0).to(device)
+        image_net = image.unsqueeze(0).to(device)
+        mask_net = mask.unsqueeze(0).to(device)
+        masked_image = masked_image.unsqueeze(0).to(device)
 
-        # take random style image
-        style_image = styles_dataset.get_image().to(device)
-        style_image_ = style_image.unsqueeze(0).to(device)
-
-        _, test_skips = netS(masked_image_)
-        test_embed, _ = netS(style_image_, False)
-        test_generated = netG(test_embed, mask_, test_skips).detach().cpu()
+        # generate new image
+        _, test_skips = netS(masked_image)
+        # test_embed, _ = netS(image_net, False)
+        test_embed, _ = netS(style_image_net, False)
+        style_code, _, _ = netM(test_embed)
+        test_generated = netG(style_code, mask_net.float(), test_skips).detach().cpu()
 
         # save result
-        a = vutils.save_image(test_generated, args.test_image_dir + str(i) + '.jpg',
-            normalize=True, save=True)
+        generated = test_generated.squeeze(0).detach().cpu().numpy()
 
+        norm_image = cv2.normalize(image_to_plot(generated), None,
+                           alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F).astype(np.uint8)
+
+        im = Image.fromarray(norm_image)
+        im.save(args.test_image_dir + '/' str(i) + '.jpg')
 
 if __name__ == '__main__':
     generate_images()
