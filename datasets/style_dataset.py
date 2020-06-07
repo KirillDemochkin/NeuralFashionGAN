eeadcc32bc03936d5bbae69351cec0fa02dfb094
Maria <36924974+MariBax@users.noreply.github.com>
2020-06-07 21:06:05 +0300

    Update style_dataset.py

--- datasets/style_dataset.py
+++ datasets/style_dataset.py
@@ -1,10 +1,3 @@
-import numpy as np
-import random
-import os
-from os.path import join
-from PIL import Image
-from torch.utils.data import Dataset
-
 class StyleDataset(Dataset):
     def __init__(self, styles_dir, transform=None):
         super().__init__()
@@ -17,20 +10,25 @@ class StyleDataset(Dataset):
 
         # randomly select style
         style = random.choice(self.styles)
-        # print(style)
+        print(style)
 
         # randomly select image of this style
         style_images_path = join(self.styles_dir, style)
         image_file = random.choice(os.listdir(style_images_path))
         image_path = join(style_images_path, image_file)
+        # image_path = 'styles/000457.jpg'
 
         # read image
         image = Image.open(image_path)
         image = np.array(image)
-        image = image.astype(np.float32) / 127.5 - 1.0
+        image = image.astype(np.float32) / 255.
+
+        width, height = image.shape[1], image.shape[0]
 
         if self.transform is not None:
             augmented = self.transform(image=image)
             image = augmented['image']
+        image *= 2
+        image -= 1
 
         return image
